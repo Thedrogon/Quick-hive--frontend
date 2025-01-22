0b82bef8f47167e21dce8457ce5cb7bdf1c75574
Sayanjit Mukherjee <sayanjitmukherjee123@gmail.com>
2025-01-23 01:38:36 +0530

    Adding assets

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Outlet , Navigate } from 'react-router-dom';
 
 const AuthLayout = () => {
+
+ const isAuthenticated=false;
   return (
-    <div>AuthLayout</div>
+    <>
+      {isAuthenticated ? (
+        <Navigate to='/'/>
+        ):(
+          <>
+          <section className='flex-1 flex flex-col items-center justify-center py-10'>
+
+            <Outlet/>
+          </section>
+          </>
+        
+      )}
+    </>
   )
 }
 
